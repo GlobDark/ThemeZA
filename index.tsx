e492624d6a6b6844260ee4af8179fd122e1a8336
Glob <jesusareinamo24@gmail.com>
2023-08-03 20:25:46 -0400

    Theme

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@/components/App';
+import { setConfig } from 'react-hot-loader';
+import './ThemeZA.css';
+
+import './i18n';
+
+setConfig({ reloadHooks: false });
+
+ReactDOM.render(<App />, document.getElementById('app'));
